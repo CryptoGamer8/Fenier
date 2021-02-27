ad906b1ec8c4283db6934f875c524c79ab1b0352
hubertzk <hubertzk@163.com>
2021-02-26 22:55:15 -0500

    slightly modify homepage

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,11 +1,9 @@
 /*back to top*/
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  var mybutton = document.getElementById("backToTop");
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
