7ca9ded7ea483905f86730c427995d120e239ef6
hubertzk <hubertzk@163.com>
2021-04-02 17:02:48 -0400

    add database CRUD demo in users and internal link example in homepage

--- Fenier/users/models.py
+++ Fenier/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(primary_key=True,max_length=15,default='dummy')
+    id = models.IntegerField('user id',auto_created=True)
+    password = models.CharField(max_length=15)
+    authority = models.IntegerField('authority')
+    created_date = models.DateTimeField('date created')
+    email = models.EmailField('user email')
+
+    def __str__(self):
+        return self.name
+
+    
--- Fenier/users/views.py
+++ Fenier/users/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from .models import Users
 
 # Create your views here.
 def index(request):
+    # create
+    # Users.objects.create(name='zk',id=0,password=111111,authority=0,created_date='2020-04-02',email='zhangkan1111@outlook.com')
+    
+    # read
+    # Users.objects.all()
+    # select_entries = Users.objects.filter(id=1) // return QuerySet 
+    
+    # update 
+    # user_zk = Users.objects.get(name='zk')     //return single object; Exception throwed.
+    # user_zk.password = 222222
+    # user_zk.save()
+
+    # delete
+    # Users.objects.filter(id=0).delete()
+    
     return render(request,'users/index.html')
