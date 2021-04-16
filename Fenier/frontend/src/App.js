3ea4f066a87a2e24d27ce07ab690a91d8e2e4566
JialinWang547 <wangjialin@northeastern.edu>
2021-04-16 13:48:10 -0400

    Add analysis charts in frontend

--- Fenier/frontend/src/App.js
+++ Fenier/frontend/src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
+import BarChart from './components/data_analysis/bar';
+import PieChart from './components/data_analysis/pie';
 
 function App() {
   
@@ -33,7 +35,9 @@ function App() {
   return (
     <div className="App">
       <iframe width="560" height="315" src={myurl} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <img src={myimg} alt="avengers"></img>
+      <img src={myimg} alt="avengers"></img><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <BarChart /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <PieChart />
     </div>
   );
 }
