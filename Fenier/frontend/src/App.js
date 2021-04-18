e8c843daa399ff01bc7c6bd5f98bef53b811c8ff
hubertzk <hubertzk@163.com>
2021-04-18 09:58:29 -0400

    deleted homepage

--- Fenier/frontend/src/App.js
+++ Fenier/frontend/src/App.js
@@ -6,8 +6,7 @@ import PieChart from './components/data_analysis/pie';
 function App() {
   return (
     <div className="App">
-      <iframe width="560" height="315" src={myurl} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-      <img src={myimg} alt="avengers"></img><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
       <BarChart /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
       <PieChart />
     </div>
